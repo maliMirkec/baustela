49d11d65065c802834e1f7d382287c874a5c2945
Star Bist <malimirkeccita@gmail.com>
2018-08-20 13:42:44 +0200

    Testing functions.

--- functions/test.js
+++ functions/test.js
@@ -1,4 +1,4 @@
-exports.handler = function (event, context, callback) {
+exports.handler = async function (event, context, callback) {
   fetch("https://api.github.com/repositories")
     .then(function (response) {
       return response.json();
