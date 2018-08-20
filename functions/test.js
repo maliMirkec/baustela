a04f8ad59f7c240452ab62741a7b1638a5738c88
Star Bist <malimirkeccita@gmail.com>
2018-08-20 13:10:22 +0200

    Testing functions.

--- functions/test.js
+++ functions/test.js
@@ -1,6 +1,10 @@
 exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
+    headers: {
+        "Access-Control-Allow-Origin" : "*",
+        'Content-Type': 'application/json'
+    },
     body: "Success!"
   })
 }
