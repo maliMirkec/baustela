2d048a6129338bbc4eadd9f0d895a09d6423e7a9
Star Bist <malimirkeccita@gmail.com>
2018-08-20 13:26:24 +0200

    Testing functions.

--- functions/test.js
+++ functions/test.js
@@ -1,11 +1,25 @@
-exports.handler = function(event, context, callback) {
-  fetch("https://baustela.silvestarbistrovic.from.hr/.netlify/functions/test").then(function(response) { return response.json(); }).then(function(myJson) { console.log(myJson); });
+exports.handler = function (event, context, callback) {
+  fetch("https://api.github.com/repositories")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (myJson) {
+      console.log(myJson);
+    });
+
+  fetch("https://baustela.silvestarbistrovic.from.hr/.netlify/functions/test")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (myJson) {
+      console.log(myJson);
+    });
 
   callback(null, {
     statusCode: 200,
     headers: {
-        "Access-Control-Allow-Origin" : "*",
-        'Content-Type': 'application/json'
+      "Access-Control-Allow-Origin": "*",
+      'Content-Type': 'application/json'
     },
     body: "Success!"
   })
